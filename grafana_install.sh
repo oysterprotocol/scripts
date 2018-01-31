c8dabe46e521b1deae16e51bea758fdb23d4558e
automyr <skycreed@gmail.com>
2018-01-31 04:22:13 +0100

    quick script to install grafana

---
+++ grafana_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#add package cloud GPG key, pipe the output to apt-key
+curl https://packagecloud.io/gpg.key | sudo apt-key add -
+#add packagecloud repo to apt sources - package cloud doesn't offer ubuntu `packages, but the debian one also works
+sudo add-apt-repository "deb https://packagecloud.io/grafana/stable/debian/ stretch main"
+#update package lists
+sudo apt-get update
+#install grafana
+sudo apt-get install grafana
+#start grafana service
+sudo systemctl start grafana-server
+#if something fails, check to see if it's running with "sudo systemctl status grafana-server"
+#enable the service to start grafana on boot
+sudo systemctl enable grafana-server.service
+
