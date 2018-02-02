30d9cf0b89a0d255fa4b532e82f3cf87fab74466
automyr <35855008+automyr@users.noreply.github.com>
2018-02-02 21:34:57 +0100

    unload tar as iota

--- updatetangle.sh
+++ updatetangle.sh
@@ -8,7 +8,7 @@ sudo pm2 stop nelson
 cd /home/iota/node/ && sudo rm -rf mainnetdb && rm -r mainnet.log && sudo mkdir mainnetdb
  
 #download the new one
-cd /tmp/ && curl -LO http://db.iota.partners/IOTA.partners-mainnetdb.tar.gz && sudo tar xzfv /tmp/IOTA.partners-mainnetdb.tar.gz -C /home/iota/node/mainnetdb && rm /tmp/IOTA.partners-mainnetdb.tar.gz
+cd /tmp/ && curl -LO http://db.iota.partners/IOTA.partners-mainnetdb.tar.gz && sudo -u iota tar xzfv /tmp/IOTA.partners-mainnetdb.tar.gz -C /home/iota/node/mainnetdb && rm /tmp/IOTA.partners-mainnetdb.tar.gz
 cd /home/ && sudo chown -R iota iota
 #restart services
 sudo service iota start
