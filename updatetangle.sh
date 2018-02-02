4bfbad89a3fce26ccfb5894609f4381edd62bb91
automyr <35855008+automyr@users.noreply.github.com>
2018-02-02 22:12:56 +0100

    run as $USER

--- updatetangle.sh
+++ updatetangle.sh
@@ -8,7 +8,7 @@ sudo pm2 stop nelson
 cd /home/iota/node/ && sudo rm -rf mainnetdb && sudo rm -rf mainnet.log && sudo mkdir mainnetdb
 cd /home/ && sudo chown -R iota iota
 #download the new one
-cd /tmp/ && curl -LO http://db.iota.partners/IOTA.partners-mainnetdb.tar.gz && sudo -u iota tar xzfv /tmp/IOTA.partners-mainnetdb.tar.gz -C /home/iota/node/mainnetdb && rm /tmp/IOTA.partners-mainnetdb.tar.gz
+cd /tmp/ && curl -LO http://db.iota.partners/IOTA.partners-mainnetdb.tar.gz && sudo tar xzfv /tmp/IOTA.partners-mainnetdb.tar.gz -C /home/iota/node/mainnetdb && rm /tmp/IOTA.partners-mainnetdb.tar.gz
 
 #restart services
 sudo service iota start
