830ef0a7edb8d69df7e2ddd9f83227d66a8dce10
automyr <skycreed@gmail.com>
2018-02-17 01:51:04 +0100

    change users

--- hk2go.sh
+++ hk2go.sh
@@ -1,16 +1,11 @@
 #On an existing ubuntu 16.04 server:
-#preempt interactive promps:
+#pre-empt interactive promps:
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -qy update
 sudo -E apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
 sudo -E apt-get -qy autoclean
 sudo apt install make
 sudo apt-get install gcc
-
-#make new hookgo user
-sudo mkdir -p /home/hookgo/
-sudo useradd -d /home/hookgo hookgo
-sudo chmod -R 777 /home/hookgo
 
 #download Go
 sudo curl -O https://storage.googleapis.com/golang/go1.9.3.linux-amd64.tar.gz
@@ -19,19 +14,19 @@ sudo mv go /usr/local/go
 sudo rm go1.9.3.linux-amd64.tar.gz
 
 #set env
-echo "export GOROOT=/usr/local/go" >> /home/hookgo/.profile
-echo "export GOPATH=/home/hookgo/go" >> /home/hookgo/.profile
-echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH/bin:/home/hookgo/" >> /home/hookgo/.profile
-source /home/hookgo/.profile
+echo "export GOROOT=/usr/local/go" >> ~/.profile
+echo "export GOPATH=/home/ubuntu/go" >> ~/.profile
+echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH/" >> ~/.profile
+source /home/ubuntu/.profile
 
 #until that works, we can do this:
 export GOROOT=/usr/local/go
-export GOPATH=/home/hookgo/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH/bin:/home/hookgo/
+export GOPATH=/home/ubuntu/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH/
 
 #Pull repo
-sudo mkdir -p /home/hookgo/go/src/github.com/oysterprotocol
-cd /home/hookgo/go/src/github.com/oysterprotocol
+sudo mkdir -p /home/ubuntu/go/src/github.com/oysterprotocol
+cd /home/ubuntu/go/src/github.com/oysterprotocol
 sudo git clone https://github.com/oysterprotocol/hooknode.git
 cd hooknode
 
@@ -50,8 +45,8 @@ cat <<EOF | sudo tee /lib/systemd/system/hooknode.service
 Description=Oyster Hooknode in Golang
 After=network.target
 [Service]
-WorkingDirectory=/home/hookgo/go/src/github.com/oysterprotocol/hooknode
-User=hookgo
+WorkingDirectory=/home/ubuntu/go/src/github.com/oysterprotocol/hooknode
+User=ubuntu
 PrivateDevices=yes
 ProtectSystem=full
 Type=simple
@@ -59,7 +54,7 @@ ExecReload=/bin/kill -HUP $MAINPID
 KillMode=mixed
 KillSignal=SIGTERM
 TimeoutStopSec=60
-ExecStart=/home/hookgo/go/src/github.com/oysterprotocol/hooknode/./bin/main.go
+ExecStart=/home/ubuntu/go/src/github.com/oysterprotocol/hooknode/./bin/main.go
 SyslogIdentifier=HOOKN
 Restart=on-failure
 RestartSec=30
@@ -67,9 +62,6 @@ RestartSec=30
 WantedBy=multi-user.target
 Alias=hooknode.service
 EOF
-
-#make sure the directories are owned by hookgo
-sudo chmod -R 774 /home/hookgo/go
 
 #start service
 sudo service hooknode start
