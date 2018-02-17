c7b7ab2ca7c5db226b1bbb373bb9967a53ccf055
automyr <skycreed@gmail.com>
2018-02-17 16:14:31 +0100

    fix permission issues

--- hk2go.sh
+++ hk2go.sh
@@ -5,7 +5,7 @@ sudo -E apt-get -qy update
 sudo -E apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
 sudo -E apt-get -qy autoclean
 sudo apt install make
-sudo apt-get install gcc
+sudo apt-get -y install gcc
 
 #download Go
 sudo curl -O https://storage.googleapis.com/golang/go1.9.3.linux-amd64.tar.gz
@@ -17,12 +17,16 @@ sudo rm go1.9.3.linux-amd64.tar.gz
 echo "export GOROOT=/usr/local/go" >> ~/.profile
 echo "export GOPATH=/home/ubuntu/go" >> ~/.profile
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH/" >> ~/.profile
-source /home/ubuntu/.profile
+source ~/.profile
 
 #until that works, we can do this:
 export GOROOT=/usr/local/go
 export GOPATH=/home/ubuntu/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH/
+
+#fix permissions#1
+sudo chmod -R 774 /home/ubuntu/go
+sudo chown -R ubuntu /home/ubuntu
 
 #Pull repo
 sudo mkdir -p /home/ubuntu/go/src/github.com/oysterprotocol
@@ -30,14 +34,18 @@ cd /home/ubuntu/go/src/github.com/oysterprotocol
 sudo git clone https://github.com/oysterprotocol/hooknode.git
 cd hooknode
 
+#fix permissions#2
+sudo chmod -R 774 /home/ubuntu/go
+sudo chown -R ubuntu /home/ubuntu
+
 # Setup ENV variables
 sudo cp .env.example .env
 
 #install dependencies for server
-sudo make install-deps
+make install-deps
 
 #make executable
-sudo go build -o ./bin/main.go
+go build -o ./bin/main.go
 
 #setup systemd service
 cat <<EOF | sudo tee /lib/systemd/system/hooknode.service
