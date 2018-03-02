a109853f0c9d85ff919fb48f84c832f38f36d4cb
automyr <skycreed@gmail.com>
2018-03-02 17:46:47 +0100

    update deploy script

--- updatehook.sh
+++ updatehook.sh
@@ -2,18 +2,44 @@
 #stop services
 sudo service iota stop
 sudo service hooknode stop
-#update repo
+#change path
 cd /home/ubuntu/go/src/github.com/oysterprotocol/hooknode
-sudo git pull
-#sudo git fetch --all
-#sudo git reset --hard origin/master
-#add temporary goenvs var
+#export goenvs
 export GOROOT=/usr/local/go
 export GOPATH=/home/ubuntu/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH/
-#install possible new dependencies
+#remove old binary file
+sudo rm -rf bin
+#download and compile updated repo
+sudo git pull
 make install-deps
-go build -o ./bin/main.go
+make build
+#change the service to point at main instead of main.go
+sudo rm -f /lib/systemd/system/hooknode.service
+cat <<EOF | sudo tee /lib/systemd/system/hooknode.service
+[Unit]
+Description=Oyster Hooknode in Golang
+After=network.target
+[Service]
+WorkingDirectory=/home/ubuntu/go/src/github.com/oysterprotocol/hooknode
+User=ubuntu
+PrivateDevices=yes
+ProtectSystem=full
+Type=simple
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=mixed
+KillSignal=SIGTERM
+TimeoutStopSec=60
+ExecStart=/home/ubuntu/go/src/github.com/oysterprotocol/hooknode/./bin/main
+SyslogIdentifier=hooknode
+Restart=on-failure
+RestartSec=30
+[Install]
+WantedBy=multi-user.target
+Alias=hooknode.service
+EOF
+#reload systemctl
+sudo systemctl daemon-reload
 #restart services
 sudo service hooknode start
 sudo service iota start
