aac669dc502fc98bdbb734a51ae8194059c16336
automyr <skycreed@gmail.com>
2018-05-14 22:54:27 +0200

    use normal jota lib

--- install_coo.sh
+++ install_coo.sh
@@ -19,13 +19,14 @@ sudo apt -qy install maven
 mkdir -p /home/$USER/coordinator_tools && cd /home/$USER/coordinator_tools
 #download testnet tools
 sudo git clone https://github.com/oysterprotocol/private-iota-testnet/ && cd private-iota-testnet
-sudo git checkout local-jota
+#testing out the normal jota lib
+#sudo git checkout local-jota
 #download iota.java.lib
-sudo git clone https://github.com/oysterprotocol/iota.lib.java/ && cd iota.lib.java && sudo git checkout oyster.iota.lib.java
+#sudo git clone https://github.com/oysterprotocol/iota.lib.java/ && cd iota.lib.java && sudo git checkout oyster.iota.lib.java
 
 ### - Compile and install both repos
 #compile and install local jota
-sudo mvn install
+#sudo mvn install
 #compile and install testnet tools
 cd ..
 sudo mvn clean package
@@ -46,7 +47,7 @@ ExecReload=/bin/kill -HUP $MAINPID
 KillMode=mixed
 KillSignal=SIGTERM
 TimeoutStopSec=60
-ExecStart=/usr/bin/java -jar iota-testnet-tools-0.9.11-for-coordinator-jar-with-dependencies.jar PeriodicCoordinator localhost 14265
+ExecStart=/usr/bin/java -jar iota-testnet-tools-0.9.10-jar-with-dependencies.jar PeriodicCoordinator localhost 14265
 SyslogIdentifier=COO
 Restart=on-failure
 RestartSec=30
